de9b840e63fe45ac266e6da36142ebb4485558b3
Félix Esqueda <jesqesc@gmail.com>
2017-08-28 09:55:25 -0500

    New version of the code has been pushed. Older versions are not working

---
+++ PendingListItems/Models/PriorityModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace PendingListItems.Models
+{
+    [Table("Priority")]
+    public class PriorityModel
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int PriorityId { get; set; }
+        
+        [Display(Name = "Priority")]
+        public string Description { get; set; }
+    }
+}
